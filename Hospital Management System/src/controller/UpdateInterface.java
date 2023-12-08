38c9f7c5ec0d5a428ddb77d534eb81402685321e
allyomega <wdonahue2012@fau.edu>
2023-12-07 22:35:49 -0500

    Fixed Javadocs again

--- Hospital Management System/src/controller/UpdateInterface.java
+++ Hospital Management System/src/controller/UpdateInterface.java
@@ -1,6 +1,23 @@
 package controller;
-
+/**
+ * UpdateInterface defines the contract for updating and deleting data.
+ * This interface for the MVC architecture of our project
+ * to handle update and delete operations on our data models.
+ */
 public interface UpdateInterface {
+
+    /**
+     * Updates a specific data entry based on row and column indices.
+     *
+     * @param row   The row index of the data entry to be updated.
+     * @param col   The column index of the data attribute to be updated.
+     * @param value The new value to update the data attribute to.
+     */
     public void update(int row,int col,String value);
+    /**
+     * Deletes a specific data entry based on its row index.
+     *
+     * @param row The row index of the data entry to be deleted.
+     */
     public void delete(int row);
 }
